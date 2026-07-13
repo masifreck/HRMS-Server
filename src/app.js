f4f2ed7f02da015ab6cdd3cf6b4769ba250c83d7
Mohd Asif <masif854380@gmail.com>
2026-07-13 15:56:20 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "HRMS Backend Running"
+    });
+});
+
+export default app;
